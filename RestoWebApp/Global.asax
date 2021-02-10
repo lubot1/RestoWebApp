2d805d8f153d12b62e41194ff9ab246f7d1fc78b
Luis Botello <botello468@gmail.com>
2021-02-10 17:12:09 -0500

    Add project files.

---
+++ RestoWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestoWebApp.MvcApplication" Language="C#" %>
---
+++ RestoWebApp/Models/Restaurants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestoWebApp.Models
+{
+    public class Restaurants
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+
+        public string RestaurantAddress { get; set; }
+
+        public string RestaurantPhone { get; set; }
+    }
+}
