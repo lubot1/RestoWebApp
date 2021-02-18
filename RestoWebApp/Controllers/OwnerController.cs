f17719a46066507c44b2dd49232587219c7b9e88
Luis Botello <botello468@gmail.com>
2021-02-17 22:37:55 -0500

    Added Owner Create, read, update functionality

---
+++ RestoWebApp/Controllers/OwnerController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using RestoWebApp.Models;
+
+namespace RestoWebApp.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+        static OwnerController()
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:44375/api/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        // GET: Owner
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Owner/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Owner/List 
+        public ActionResult List()
+        {
+            string url = "ownerdata/getowners";
+            HttpResponseMessage httpResponse = client.GetAsync(url).Result;
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                IEnumerable<OwnerDto> OwnerList = httpResponse.Content.ReadAsAsync<IEnumerable<OwnerDto>>().Result;
+                return View(OwnerList);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Owner/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Owner/Create
+        [HttpPost]
+        public ActionResult Create(Owner OwnerInfo)
+        {
+            string url = "ownerdata/create";
+            HttpContent content = new StringContent(jss.Serialize(OwnerInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage httpResponse = client.PostAsync(url, content).Result;
+
+            if(httpResponse.IsSuccessStatusCode)
+            {
+                int OwnerID = httpResponse.Content.ReadAsAsync<int>().Result;
+                return RedirectToAction("Details", new { id = OwnerID });
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Owner/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Owner/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Owner/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Owner/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RestoWebApp/Controllers/OwnerDataController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestoWebApp.Models;
+
+namespace RestoWebApp.Controllers
+{
+    public class OwnerDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/OwnerData/GetOwners
+        [HttpGet]
+        public IEnumerable<OwnerDto> GetOwners()
+        {
+            List<Owner> Owners = db.Owners.ToList();
+            List<OwnerDto> OwnerDtos = new List<OwnerDto> { };
+
+            foreach(var Owner in Owners)
+            {
+                OwnerDto NewOwner = new OwnerDto
+                {
+                    OwnerID = Owner.OwnerID,
+                    OwnerFirstName = Owner.OwnerFirstName,
+                    OwnerLastName = Owner.OwnerLastName
+                };
+                OwnerDtos.Add(NewOwner);
+            }
+            return OwnerDtos;
+        }
+
+        // GET: api/OwnerData/GetOwner/5
+        [ResponseType(typeof(Owner))]
+        [HttpGet]
+        public IHttpActionResult GetOwner(int id)
+        {
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(owner);
+        }
+
+        // PUT: api/Owners/UpdateOwner/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateOwner(int id, [FromBody]Owner owner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != owner.OwnerID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(owner).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OwnerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Owners/AddOwner
+        [ResponseType(typeof(Owner))]
+        [HttpPost]
+        public IHttpActionResult AddOwner([FromBody]Owner owner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Owners.Add(owner);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = owner.OwnerID }, owner);
+        }
+
+        // DELETE: api/Owners/5
+        [ResponseType(typeof(Owner))]
+        public IHttpActionResult DeleteOwner(int id)
+        {
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            db.Owners.Remove(owner);
+            db.SaveChanges();
+
+            return Ok(owner);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OwnerExists(int id)
+        {
+            return db.Owners.Count(e => e.OwnerID == id) > 0;
+        }
+    }
+}
